1078235be14a091e2422e2846d53b753fd19e4bf
Tibor Lovas <info@tiborlovas.com>
2013-12-13 00:42:29 +0100

    first commit

---
+++ app/scripts/script.js
@@ -0,0 +1,61 @@
+$(function(){
+	/* Parallax job */
+	$.stellar({
+		horizontalScrolling: false
+	});
+	
+	/* Retina images swap */
+	var retina = window.devicePixelRatio > 1;
+	if (retina) {
+	    $('img.retina').each(function(){
+		    $(this).attr('src', $(this).attr('src').replace('.jpg', '@2x.jpg'));
+	    });
+	}
+	
+	/* Hero autoresize */ 
+	$(window).resize(function() {
+		$(".main").innerHeight($(window).height());
+	});
+	$(window).resize();
+
+    /* Global Sticky Elements Trigger */
+    $('.sticky-element').addClass('invisible');
+    $('.sticky-trigger').waypoint(function(direction) {
+    		//alert(direction);
+            $('.sticky-element').toggleClass('invisible');
+        }, {offset: $('.sticky-element').outerHeight()+1}
+    );
+    
+    /* Smooth scrolling of links between panels */
+/*
+	var $panels = $('.topbar');
+		
+	$panels.each(function() {
+	  var $panel = $(this);
+	  var hash = '#' + this.id;
+	  $('a[href="' + hash + '"]').click(function(event) {
+		alert('ok');
+	    $scrollElement.stop().animate({
+	      scrollLeft: $panel.offset().left
+	    }, 500, 'swing', function() {
+	      window.location.hash = hash;
+	    });
+	
+	    event.preventDefault();
+	  });
+	});
+*/
+	
+	/* Docs nav highlighting */
+/*
+	$('section')
+	.waypoint(function(direction) {
+		var $links = $('a[href="#' + this.id + '"]');
+		$links.parent().toggleClass('active', direction === 'down');
+	});
+*/
+	
+});
+
+/* Hero autoresize needs to be made by javascript to avoid blink caused by jquery load latency */
+//document.getElementsByClassName("main")[0].style.height = window.innerHeight;
